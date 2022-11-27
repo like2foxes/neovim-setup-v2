e35cab236fef1ceb710c62547d9468c52f5f3ae6
Shai Cioara <shai@mreshet.co.il>
2022-11-28 00:43:02 +0200

    Added some shortcuts for telescope navigation

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,1 +1,21 @@
-require('telescope').setup()
+local actions = require('telescope.actions')
+require('telescope').setup({
+	  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default + actions.center
+			},
+			n = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+			}
+		}
+	}
+})
