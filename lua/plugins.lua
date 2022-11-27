937838054f2cb8ef8c65934960f7850931e21ee2
Shai Cioara <shai@mreshet.co.il>
2022-11-27 11:56:12 +0200

    Added git signs and lazygit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -36,6 +36,8 @@ return require('packer').startup(function(use)
 	use 'hrsh7th/vim-vsnip'
 
 	use 'windwp/nvim-autopairs'
+	use 'lewis6991/gitsigns.nvim'
+	use 'kdheepak/lazygit.nvim'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
