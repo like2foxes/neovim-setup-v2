f069a92a011eb8774b471baba60eaf541f71950e
Shai Cioara <shai@mreshet.co.il>
2022-11-27 11:57:10 +0200

    Loaded some imports

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
 require('plugins')
 require('options')
+require('neodev-config') -- must be set before lsp-config
 require('lsp-config')
 require('cmp-config')
+require('tree-sitter-config')
+require('lsp-saga-config')
+require('telescope-config')
+require('autopairs-config')
 require('themes/newpaper')
 require('keymaps')
+require('gitsigns-config')
+require('which-key-config')
 
