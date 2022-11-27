1a65860190e4588b8335103c5a9f9629b366b61a
Shai Cioara <shai@mreshet.co.il>
2022-11-27 11:56:36 +0200

    Added some keymappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,16 @@
 local map = require("utils").map
 
+-- set leader as space
+map('','<Space>','<NOP>')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- better exit from insert mode
 map("i", "kj", "<Esc>")
 map("i", "jk", "<Esc>")
+
+-- better moving between windows
+map('n','<C-h>','<C-w>h')
+map('n','<C-j>','<C-w>j')
+map('n','<C-k>','<C-w>k')
+map('n','<C-l>','<C-w>l')
