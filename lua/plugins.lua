72f0cf69eeb672d152bfeae457fedf938cdfc901
Shai Cioara <shai@mreshet.co.il>
2022-11-28 13:42:24 +0200

    Added symobls outline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,6 +43,7 @@ return require('packer').startup(function(use)
 	use 'nvim-tree/nvim-tree.lua'
 	use 'akinsho/bufferline.nvim'
 	use 'nvim-lualine/lualine.nvim'
+	use 'simrat39/symbols-outline.nvim'
 	use 'NvChad/nvim-colorizer.lua'
 	use 'karb94/neoscroll.nvim'
 	use 'numToStr/Comment.nvim'
