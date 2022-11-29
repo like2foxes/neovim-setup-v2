1da89186e3b606b572c1ee0f8edc83b5413c60bd
Shai Cioara <shai@mreshet.co.il>
2022-11-29 15:00:34 +0200

    Tweeked autopairs

--- lua/autopairs-config.lua
+++ lua/autopairs-config.lua
@@ -1,1 +1,13 @@
-require('nvim-autopairs').setup {}
+require('nvim-autopairs').setup {
+	enable_check_bracket_line = false,
+	fast_wrap = {
+		map = '<M-e>',
+		chars = { '{', '[', '(', '"', "'" },
+		pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+		end_key = '$',
+		keys = 'qwertyuiopzxcvbnmasdfghjkl',
+		check_comma = true,
+		highlight = 'Search',
+		highlight_grey='Comment'
+	},
+}
