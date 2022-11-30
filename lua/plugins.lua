9c1bcf5250204a47f292a00b7fe8db8f710e4572
Shai Cioara <shai@mreshet.co.il>
2022-11-30 14:09:37 +0200

    Changed packer to work in a popup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,7 +9,31 @@ local ensure_packer = function()
 	return false
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 local packer_bootstrap = ensure_packer()
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end
+    }
+  }
+)
 
 return require('packer').startup(function(use)
 
@@ -77,3 +101,4 @@ return require('packer').startup(function(use)
 		require('packer').sync()
 	end
 end)
+
