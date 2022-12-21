716af0ffd6193d82e79fb7436f3be1eb58ab0425
Shai Cioara <shai@mreshet.co.il>
2022-12-21 15:06:46 +0200

    Added keys for treesitter navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -70,6 +70,21 @@ local mappings = {
 	["?"] = {require('telescope.builtin').oldfiles, "[?] Find recently opened files" },
 	[" "] = {require('telescope.builtin').buffers, '[ ] Find existing buffers' },
 	['/'] = { function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, }) end, "[/] Fuzzily search in current buffer]"},
+	['f'] = {require('telescope.builtin').find_files, "[F]iles" },
+	j = {
+		name = "[J]ump to Next:",
+		f = {":TSTextobjectGotoNextStart @function.outer<CR>", "[F]unction"},
+		c = {":TSTextobjectGotoNextStart @conditional.outer<CR>", "[C]onditional"},
+		b = {":TSTextobjectGotoNextStart @block.outer<CR>", "[B]lock"},
+		l = {":TSTextobjectGotoNextStart @loop.outer<CR>", "[L]oop"},
+		i = {
+			name = "[I]nner",
+			f = {":TSTextobjectGotoNextStart @function.inner<CR>", "[F]unction"},
+			c = {":TSTextobjectGotoNextStart @conditional.inner<CR>", "[C]onditional"},
+			b = {":TSTextobjectGotoNextStart @block.inner<CR>", "[B]lock"},
+			l = {":TSTextobjectGotoNextStart @loop.inner<CR>", "[L]oop"},
+		},
+	},
 	s = {
 		name = "[S]earch",
 		f = {require('telescope.builtin').find_files, "[F]iles" },
